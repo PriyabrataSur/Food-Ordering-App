c9cdaaffe2e18a31ca87b56059bfe4ea50220de7
PriyabrataSur <priyabratasur1311@gmail.com>
2025-08-01 22:30:01 +0530

    Food Ordering App

---
+++ js/table.js
@@ -0,0 +1,6 @@
+const accountId ="34535423345";
+let accountState = "West bengal";
+let accountEmail = "abc@gmail.com";
+const accountPassword = "123456789";
+let accountCity = "Chandernagore";
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity]);
